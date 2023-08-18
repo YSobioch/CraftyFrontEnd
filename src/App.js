8e7da84d4c912012fe56cf67f152febc32c76589
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 <YuriSobioch@gmail.com>
2023-08-17 20:32:54 -0600

    changed app css

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { createStore } from "redux";
 import { Provider } from "react-redux";
 import { reducer } from './redux/storeState'
 
-import './app.css'
+import 'app.css'
 
 const store = createStore(reducer);
 
